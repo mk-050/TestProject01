73562c4f577296fe109ce26116c5bcb5cbfb2f7b
mk-050 <yoshikawa.0505@outlook.jp>
2024-05-12 16:41:29 +0900

    Tanaka

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,8 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 	
-    String str1 ="鈴木";
-    System.out.println(str1);
+    String str2 ="Tanaka";
+    System.out.println(str2);
 	}
 
 }
