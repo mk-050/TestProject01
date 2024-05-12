f7b04e07d9217bbd017173832efd705fed9289fd
mk-050 <yoshikawa.0505@outlook.jp>
2024-05-12 20:21:52 +0900

    後置加算子

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,12 @@
-
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
 	
-	int a = 6%3;
-    System.out.println(a);
+	int a = 10;
+    System.out.println(a++);
+    System.out.println(a++);
+    System.out.println(a++);
+    System.out.println(a++);
     }
 	
 }
