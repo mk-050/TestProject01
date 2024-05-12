c0fdf2b2ef23de13ce2ed726df97c88fa92c65d1
mk-050 <yoshikawa.0505@outlook.jp>
2024-05-12 17:43:39 +0900

    int a = 6%3;

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,8 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 	
-    double a2 = (double)10/3;
-    System.out.println(a2);
+	int a = 6%3;
+    System.out.println(a);
     }
 	
 }
