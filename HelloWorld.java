0ca96fb40c258d3b1b13fd27411fad406a00d545
mk-050 <yoshikawa.0505@outlook.jp>
2024-05-12 17:36:39 +0900

    float a1 = (float)10/3;

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,8 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 	
-    float a = (float)10/3;
-    System.out.println(a);
+    float a1 = (float)10/3;
+    System.out.println(a1);
     }
 	
 }
