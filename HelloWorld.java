f10149876232efdb98f8e62d8f074bfc726464aa
mk-050 <yoshikawa.0505@outlook.jp>
2024-05-14 21:13:24 +0900

    aは0でも1でもない

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,17 +3,19 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		int age = 88;
-		if (age <= 100) {
-			if (age < 18) {
-				System.out.println("未成年");
-			} else if (age == 77) {
-				System.out.println("喜寿");
-			} else if (age == 88) {
-				System.out.println("米寿");
-			} else {
-				System.out.println("成人");
-			}
+		int a = 5;
+
+		switch (a) {
+		case 0:
+			System.out.println("aは0に等しい");
+			break;
+
+		case 1:
+			System.out.println("aは1に等しい");
+			break;
+
+		default:
+			System.out.println("aは0でも1でもない");
 		}
 
 	}
