ac1967b78f5468e7807616233f0a38cef03083d0
mk-050 <yoshikawa.0505@outlook.jp>
2024-05-14 22:03:40 +0900

    加藤

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,9 +3,12 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		char[]c= {'x','y','z'};
+		String[]name= {"田中","高橋","斉藤"};
 		
-		System.out.println(c[0]);
+		name[0]="加藤";
+		
+		System.out.println(name[0]);
+		
 
 	}
 }
