ae4be93b4b485eb88e87f5456de8cd1b5023a210
mk-050 <yoshikawa.0505@outlook.jp>
2024-05-15 21:35:17 +0900

    for分 ★

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,9 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		for (int i = 1; i < 10; i += 2) {
-			System.out.println(i);
+		for (int i = 1; i <=5; i++) {
+			System.out.println("★");
 		}
-
 	}
 }
