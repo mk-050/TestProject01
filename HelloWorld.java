5ae8a3c40da1e546d2400638c4a157c1d421e140
mk-050 <yoshikawa.0505@outlook.jp>
2024-05-16 21:39:08 +0900

    条件演算子

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,10 @@
 
 public class HelloWorld {
-
 	public static void main(String[] args) {
 
-		for (int i = 1; i <=5; i++) {
-			System.out.println("★");
-		}
+		int a = 10;
+		String b = a >= 0 ? "プラス" : "マイナス";
+		System.out.println(b);
+
 	}
 }
