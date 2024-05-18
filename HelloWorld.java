9ff1404a5f70bbe0a8cbdddda32df0321f5033c0
mk-050 <yoshikawa.0505@outlook.jp>
2024-05-18 20:28:26 +0900

    while文　基礎

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,11 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 
-		for (int i = 0; i <= 5; i++) {
-			String b = i % 2 == 0 ? "☆" : "★";
-			System.out.println(b);
-		}
+		int i = 1;
 
+		while (i<=5) {
+			System.out.println("★");
+			i++;
+		}
 	}
 }
